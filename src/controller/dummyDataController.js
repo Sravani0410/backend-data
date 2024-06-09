d4b0be0c2c867163f8177270c3e819947d320043
sravani <sandhyapyla.8@gmail.com>
2024-06-09 05:43:55 +0530

    add and modify some data

--- src/controller/dummyDataController.js
+++ src/controller/dummyDataController.js
@@ -1,7 +1,17 @@
 const data=require ("../db.json")
+const data=require ("../services.json")
 
 
 exports.getDummyData = async (req, res) => {
+    try {
+      return res.status(200).json(data);
+    } catch (err) {
+     return  res.status(500).json(err);
+    }
+};
+
+
+exports.getDummyServiceData = async (req, res) => {
     try {
       return res.status(200).json(data);
     } catch (err) {
--- src/route.js
+++ src/route.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const dummyDataController=require("./controller/dummyDataController")
-
 router.get('/data', dummyDataController.getDummyData);
-
+router.get('/services',dummyDataController.getDummyServiceData)
 module.exports = router
